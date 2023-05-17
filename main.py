e349632bb217e4dc7e5575d326ed535594318e5a
saracsilva <saracarolinasilva@gmail.com>
2023-05-17 23:00:22 +0200

    added input, button and download function

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ import customtkinter
 from pytube import YouTube
 
 
+def startDownload():
+    try:
+        ytLink = link.get()
+        ytObject = YouTube(ytLink)
+        video = ytObject.streams.get_highest_resolution()
+        video.download()
+    except:
+        print("Youtube link is invalid.")
+    print("Download complete")
+
+
 # System settings
 
 customtkinter.set_appearance_mode("System")
@@ -13,6 +24,22 @@ customtkinter.set_default_color_theme("blue")
 app = customtkinter.CTk()
 app.geometry("720x480")
 app.title("Youtube Downloader")
+
+# UI elements
+
+title = customtkinter.CTkLabel(app, text="Insert an youtube link")
+title.pack(padx=10, pady=10)
+
+# Link Input
+
+url_var = tkinter.StringVar()
+link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
+link.pack()
+
+
+# Download Button
+
+download = customtkinter.CTkButton(app, text="Download", command=startDownload)
 
 
 # Run app
